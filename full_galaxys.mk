8603d94adcc5ae8dd575ebdc46d2a5aa13609670
codeworkx <daniel.hillenbrand@codeworkx.de>
2010-11-29 19:58:31 +0100

    added iptables kernel modules

--- full_galaxys.mk
+++ full_galaxys.mk
@@ -111,6 +111,13 @@ PRODUCT_COPY_FILES += \
     device/samsung/galaxys/prebuilt/etc/vold.fstab:system/etc/vold.conf \
     device/samsung/galaxys/prebuilt/lib/egl/egl.cfg:system/lib/egl/egl.cfg
 
+# iptables kernel modules
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxys/prebuilt/lib/modules/2.6.32.9/iptable_filter.ko:system/lib/modules/2.6.32.9/iptable_filter.ko \
+    device/samsung/galaxys/prebuilt/lib/modules/2.6.32.9/iptable_mangle.ko:system/lib/modules/2.6.32.9/iptable_mangle.ko \
+    device/samsung/galaxys/prebuilt/lib/modules/2.6.32.9/iptable_nat.ko:system/lib/modules/2.6.32.9/iptable_nat.ko \
+    device/samsung/galaxys/prebuilt/lib/modules/2.6.32.9/iptable_raw.ko:system/lib/modules/2.6.32.9/iptable_raw.ko \
+    device/samsung/galaxys/prebuilt/lib/modules/2.6.32.9/ip_tables.ko:system/lib/modules/2.6.32.9/ip_tables.ko
 
 # Kernel
 ifeq ($(TARGET_PREBUILT_KERNEL),)
