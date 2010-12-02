a9755f8c7840ff41dc217dd578173f51c5dd9525
Daniel Hillenbrand <info@codeworkx.de>
2010-12-02 04:19:37 -0800

    ANDROID_ARM_LINKER := true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,6 +44,7 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8
 TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8
+ANDROID_ARM_LINKER := true
 
 # Sound related defines
 BOARD_USES_ALSA_AUDIO := true
