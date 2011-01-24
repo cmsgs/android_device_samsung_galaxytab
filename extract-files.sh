25bfb9f518c386ad72eb75602d9b156f8b3eb0b0
technomancer <dominikbehr@yahoo.com>
2011-01-23 21:05:15 -0800

    use gralloc from JK5 after all
    Change-Id: I2de5fcf20beadd4a88912dd3abb8e7724917226f

--- extract-files.sh
+++ extract-files.sh
@@ -234,7 +234,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/__DEVICE__/proprietary/lib/libsrv_um.so:system/lib/libsrv_um.so \
     vendor/samsung/__DEVICE__/proprietary/lib/libsrv_init.so:system/lib/libsrv_init.so \
     vendor/samsung/__DEVICE__/proprietary/lib/libglslcompiler.so:system/lib/libglslcompiler.so \
-    vendor/samsung/__DEVICE__/proprietary/crespo_egl/lib/hw/gralloc.s5pc110.so:system/lib/hw/gralloc.s5pc110.so
+    vendor/samsung/__DEVICE__/proprietary/lib/hw/gralloc.s5pc110.so:system/lib/hw/gralloc.s5pc110.so
 
 #PRODUCT_COPY_FILES += \
 #    vendor/samsung/__DEVICE__/proprietary/crespo_egl/bin/pvrsrvinit:system/bin/pvrsrvinit \
